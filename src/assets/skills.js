a9e987354f6b2be81001769c493314b6828ff1a1
darafsa <marek1999@online.de>
2023-07-01 15:33:00 +0200

    skills

---
+++ src/assets/skills.js
@@ -0,0 +1,20 @@
+export const skills = {
+	Acrobatics: {
+		time: 'One Round',
+		spezializations: {
+			description: 'Type of acrobatics used, form or style',
+			examples: ['diving', 'trapeze', 'tumbling'],
+		},
+		description: {
+			main:
+				'Characters with this skill can tumble, leap, and roll to avoid falling damage, to entertain an audience, or to surprise an opponent in combat.',
+			extra: `Using acrobatics can improve many of a character's climbing/jumping and running attempts. The gamemaster determines the difficulty of the acrobatics stunt. One-half of the difference (rounded up) between the difficulty and the acrobatics roll is added to the complementary skill. The acrobatics attempt and the climbing/jumping or running try must be done on the same turn, incurring a multiple action penalty.<br>
+			Instead of adding a modifier to the running or swim difficulty for particularly challenging obstacle courses, the gamemaster may have the hero make an acrobatics roll in addition to a running or swim roll.<br>
+			Acrobatics can make a character appear more intimidating. The player may add one-half of the difference (rounded up) between the difficulty and the successful acrobatics roll to their intimidation attempt. The intimidating attempt may be made on the same turn as the acrobatics roll or on the next round.<br>
+			During a fall, acrobatics may be used to reduce bodily harm. If the character has not already used his declared actions for the round, he may rely on acrobatics in an attempt to land properly as a "full reaction". The character generates an acrobatics total. For every five points over the base difficulty number of 10, the hero reduces the damage total by one point, in addition to a base reduction of one point. The damage total, however, cannot be lower than zero.<br>
+			The difficulty depends upon how difficult the acrobatic maneuver is, some examples are provided below:<br>
+			... [!!!]
+			`,
+		},
+	},
+}
